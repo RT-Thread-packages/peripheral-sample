ba1f633808e8e41fca4b48d8c4342ae63cae1a2c
misonyo <misonyo@foxmail.com>
2019-04-01 14:20:05 +0800

    [更新]移除定时器设置频率代码

--- hwtimer_sample.c
+++ hwtimer_sample.c
@@ -22,7 +22,6 @@
 /* 定时器超时回调函数 */
 static rt_err_t timeout_cb(rt_device_t dev, rt_size_t size)
 {
-    rt_kprintf("this is hwtimer timeout callback fucntion!\n");
     rt_kprintf("tick is :%d !\n", rt_tick_get());
 
     return 0;
@@ -34,7 +33,6 @@ static int hwtimer_sample(int argc, char *argv[])
     rt_hwtimerval_t timeout_s;      /* 定时器超时值 */
     rt_device_t hw_dev = RT_NULL;   /* 定时器设备句柄 */
     rt_hwtimer_mode_t mode;         /* 定时器模式 */
-    rt_uint32_t freq = 10000;       /* 计数频率 */
 
     /* 查找定时器设备 */
     hw_dev = rt_device_find(HWTIMER_DEV_NAME);
@@ -54,14 +52,6 @@ static int hwtimer_sample(int argc, char *argv[])
 
     /* 设置超时回调函数 */
     rt_device_set_rx_indicate(hw_dev, timeout_cb);
-
-    /* 设置计数频率(默认1Mhz或支持的最小计数频率) */
-    ret = rt_device_control(hw_dev, HWTIMER_CTRL_FREQ_SET, &freq);
-    if (ret != RT_EOK)
-    {
-        rt_kprintf("set frequency failed! ret is :%d\n", ret);
-        return ret;
-    }
 
     /* 设置模式为周期性定时器 */
     mode = HWTIMER_MODE_PERIOD;
